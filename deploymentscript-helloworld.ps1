83e3ccfb77074d88c26f80fb039835e6756fdee9
chrisbaudo <chrisbaudo@hotmail.com>
2020-03-31 10:50:29 -0500

    Update deploymentscript-helloworld.ps1

--- deploymentscript-helloworld.ps1
+++ deploymentscript-helloworld.ps1
@@ -1,5 +1,1 @@
-param([string] $name)
-$output = 'Hello {0}' -f $name
-Write-Output $output
-$DeploymentScriptOutputs = @{}
-$DeploymentScriptOutputs['text'] = $output
+Install-Module PowerShellGet –Repository PSGallery –Force
