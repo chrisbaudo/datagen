ac4a301105b955025fedbe8d7c5ee7ac42b1e104
chrisbaudo <chrisbaudo@hotmail.com>
2020-03-31 13:22:26 -0500

    Update deploymentscript-datalakeconfig.ps1

--- deploymentscript-datalakeconfig.ps1
+++ deploymentscript-datalakeconfig.ps1
@@ -1,7 +1,7 @@
 Install-Module PowerShellGet –Repository PSGallery –Force
 Install-Module Az.Storage -RequiredVersion 1.13.3-preview -Repository PSGallery -AllowClobber -AllowPrerelease -Force
 
-$ctx = New-AzStorageContext -StorageAccountName 'stmdwdevscus001' -UseConnectedAccount
+$ctx = New-AzStorageContext -StorageAccountName $storageaccountname -UseConnectedAccount
 
 $filesystemName = "raw"
 New-AzDatalakeGen2FileSystem -Context $ctx -Name $filesystemName
